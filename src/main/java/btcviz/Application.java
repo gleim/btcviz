1bb6e514c7ac5c43c81b7a25d0d230a9d4a726bc
Bill Gleim <bill@sprint.ly>
2014-03-09 11:36:00 -0700

    Upgrade & simplify property graph creation

--- src/main/java/btcviz/Application.java
+++ src/main/java/btcviz/Application.java
@@ -8,47 +8,69 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.CanReadFileFilter;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.index.UniqueFactory;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
-import org.springframework.data.neo4j.core.GraphDatabase;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-@Configuration
-@EnableNeo4jRepositories
-public class Application extends Neo4jConfiguration implements CommandLineRunner {
+public class Application {
 
     private static final String DB_PATH = "db/btcvizneo4j.db";
 
-    @Bean
-    EmbeddedGraphDatabase graphDatabaseService() {
-        return new EmbeddedGraphDatabase( DB_PATH );
+    private enum AccountRelationshipTypes implements RelationshipType
+    {
+        TRANSACTS_WITH
     }
 
-    @Autowired
-    PersonRepository personRepository;
+    private static GraphDatabaseService graphDb;
 
-    @Autowired
-    GraphDatabase graphDatabase;
+    public static void run(String... args) throws Exception {
 
-    public void run(String... args) throws Exception {
+        // create property graph database
+        graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
 
-        Transaction tx = graphDatabase.beginTx();
-        try {
+        // register shutdown hook to prevent database corruption upon interruption
+        registerShutdownHook( graphDb );
 
+        try ( Transaction tx = graphDb.beginTx() ) {
+            // add uniqueness constraint
+            graphDb.schema()
+                            .constraintFor( DynamicLabel.label("Account"))
+                            .assertPropertyIsUnique("name")
+                            .create();
+
+            // commit schema constraint
+            tx.success();
+        }
+
+        try ( Transaction tx = graphDb.beginTx() ) {
+
+            UniqueFactory<Node> accountFactory = new UniqueFactory.UniqueNodeFactory( graphDb, "accounts")
+            {
+                @Override
+                protected void initialize( Node created, Map<String, Object> properties )
+                {
+                    created.addLabel( DynamicLabel.label( "Account" ) );
+                    created.setProperty( "address", properties.get( "address" ) );
+                }                  
+            };
+            int records = 0;
+    
             Collection<File> files = getFolderContents("./json");
 
             for (Iterator<File> iter = files.iterator(); iter.hasNext();)
@@ -132,30 +154,47 @@ public class Application extends Neo4jConfiguration implements CommandLineRunner
                                         }
                                     }
 
-                                    for (String in_addr : input_addresses) {
-                                        Person in = new Person(in_addr);
+                                    if ((input_addresses.size() > 0) && (output_addresses.size() > 0)) {
+                                        System.out.println("Inputs:  " + input_addresses.size());
+                                        System.out.println("Outputs: " + output_addresses.size());
 
-                                        for (String out_addr : output_addresses) {
-                                            Person out = new Person(out_addr);
-
-                                            personRepository.save(out);
-
-                                            in.transactsWith(out);
-                                            personRepository.save(in);
-
-                                            System.out.println(in_addr + " transacts with " + out_addr);
+                                        // limit node relationships during preliminary analysis
+                                        if (output_addresses.size() > 500) {
+                                            System.out.println("Break before committing records");
+                                            continue;
                                         }
-                                    }
+                                        for (String in_addr : input_addresses) {
+                                            //Account in = new Account(in_addr);
 
+                                            for (String out_addr : output_addresses) {
+
+                                                // create property graph representation of entity associated with transaction input
+                                                Node inputNode = accountFactory.getOrCreate( "address", in_addr );
+
+                                                // create property graph representation of entity associated with transaction output
+                                                Node outputNode = accountFactory.getOrCreate( "address", out_addr );
+                                     
+                                                // create property graph representation of transaction relationship
+                                                Relationship relationshipA = inputNode.createRelationshipTo( outputNode, AccountRelationshipTypes.TRANSACTS_WITH );
+
+                                                // create details of transaction relationship
+                                                //relationshipA.setProperty( "timestamp", timestamp );
+
+                                                System.out.println(in_addr + " transacts with " + out_addr);
+
+                                                records++;
+                                            }
+                                        }
+                                        System.out.println(records + " interactions processed.");
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                // commit the transactions to the database
+                tx.success();
             }
-            tx.success();
-        } finally {
-            tx.finish();
         }
     }
 
@@ -178,7 +217,22 @@ public class Application extends Neo4jConfiguration implements CommandLineRunner
     public static void main(String[] args) throws Exception {
         org.neo4j.kernel.impl.util.FileUtils.deleteRecursively(new File( DB_PATH ));
 
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
+        Application.run(args);
     }
 
+    private static void registerShutdownHook( final GraphDatabaseService graphDb )
+    {
+        // Registers a shutdown hook for the Neo4j instance so that it
+        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+        // running application).
+        Runtime.getRuntime().addShutdownHook( new Thread()
+        {
+            @Override
+            public void run()
+            {
+                graphDb.shutdown();
+            }
+        } );
+    }
 }
